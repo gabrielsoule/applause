a5cbdfa30d2f9b6ff9c1186d281eecad529763bc
Gabriel Soule <gabesoule@gmail.com>
2025-10-25 14:10:00 -0700

    oops, forgot to git add the new Json wrapper around nhlomann/json

---
+++ applause/util/Json.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <nlohmann/json.hpp>
+
+namespace applause {
+
+/**
+ * @brief Type alias for nlohmann::json under the applause namespace.
+ *
+ * License: nlohmann/json is MIT licensed. Thanks for the brilliant work!
+ * @see https://github.com/nlohmann/json
+ */
+using json = nlohmann::json;
+
+}  // namespace applause
